be0ff4780be2c2d9bc3afd37912dd5143d06fe7a
manoel.acmn <manoel.acmn@puccampinas>
2022-10-21 17:06:26 -0300

    add oracle db

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
 const express = require("express");
 const app = express();
 const port = 8111;
+const oracledb = require('oracledb');
+
 
 
 app.use(express.static('public'))
 
 
+async function run(ID) {
+
+   let connection;
+ 
+   try {
+ 
+     connection = await oracledb.getConnection({ user: "system", 
+     password: "system", 
+     connectionString: "localhost:1521/xe" });
+ 
+     console.log("Successfully connected to Oracle Database");
+ 
+     // Create a table
+ 
+     await connection.execute(`insert into BILHETE values (${ID},current_timestamp)`); 
+ 
+     // Insert some data
+ 
+     connection.commit();
+ 
+   } catch (err) {
+     console.error(err);
+   } finally {
+     if (connection) {
+       try {
+         await connection.close();
+       } catch (err) {
+         console.error(err);
+       }
+     }
+   }
+ }
 app.get("/", (req, res) => {
   res.sendFile(__dirname + '/indexGerar.html')
 });
@@ -16,6 +50,7 @@ app.listen(port, () => {
 });
 
 app.post('/generate',(req,res) => {
-  ID = Math.floor(Math.random() * 10000) + 1;    
+  ID = Math.floor(Math.random() * 100000) + 1;    
   console.log("ID:%i",ID);
+  run(ID);
 })
