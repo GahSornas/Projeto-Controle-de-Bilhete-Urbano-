7fbdf2538de198aa09e3196de2075ddf9c0def73
Gabriel <gabrielbsornas@gmail.com>
2022-11-18 17:36:08 -0300

    concertos

--- public/recharge.js
+++ public/recharge.js
@@ -21,20 +21,3 @@ function abrirPopup(id) {
 function fecharPopup(id) {
   document.getElementById(id).style.display = "none";
 }
-// document.getElementById("btnRecarga").onclick = 
-// async function () {
-//     value= document.getElementById('campoBilheteRecarga').outerHTML;
-//     fetch('/recharge',{
-//         method:'POST',
-//         id : value,
-//         kindID : "unico"
-//     })
-//         .then(response => response.json())
-//         .then(response => document.getElementById("showID").innerHTML = response['id'])
-//         .catch(error => console.log(error))
-//         }
-
-document.button.getElementsByClassName("btnRecarga").onclick =
- function(){
-    console.log("")
-}
