d604fcfed744998a2c8ef4f212717fde8161b908
Gabriel <gabrielbsornas@gmail.com>
2022-11-18 16:43:45 -0300

    popup

--- public/recharge.js
+++ public/recharge.js
@@ -1,3 +1,26 @@
+// document.getElementById("btnRecarga").onclick =
+// async function () {
+//     value= document.getElementById('campoBilheteRecarga').outerHTML;
+//     fetch('/recharge',{
+//         method:'POST',
+//         id : value,
+//         kindID : "unico"
+//     })
+//         .then(response => response.json())
+//         .then(response => document.getElementById("showID").innerHTML = response['id'])
+//         .catch(error => console.log(error))
+//         }
+
+document.button.getElementsByClassName("btnRecarga").onclick = function () {
+  console.log("");
+};
+
+function abrirPopup(id) {
+  document.getElementById(id).style.display = "block";
+}
+function fecharPopup(id) {
+  document.getElementById(id).style.display = "none";
+}
 // document.getElementById("btnRecarga").onclick = 
 // async function () {
 //     value= document.getElementById('campoBilheteRecarga').outerHTML;
