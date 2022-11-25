7ef0af908d7fbec5497b3831e3f62627c0ec63ed
Gabriel <gabrielbsornas@gmail.com>
2022-11-25 09:38:48 -0300

    fixed app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const oracledb = require('oracledb');
-const bodyParser = require('body-parser');
-const db = require(__dirname+ '/dbActions.js');
-
+const oracledb = require("oracledb");
+const bodyParser = require("body-parser");
+const db = require(__dirname + "/dbActions.js");
 
 const dbCredentials = {
   user: "PI",
@@ -38,10 +37,9 @@ app.get("/Relatorio", (req, res) => {
   res.sendFile(__dirname + "/public/indexRelatorio.html");
 });
 
-
 app.listen(port, () => {
   console.log("Servidor Rodando.");
-  console.log(`127.0.0.1:${port}`)
+  console.log(`127.0.0.1:${port}`);
 });
 
 app.post("/generate", async (req, res) => {
@@ -60,8 +58,7 @@ app.post("/generate", async (req, res) => {
 
 //Recarga
 
-
-app.post('/recharge', async (req,res) => {
+app.post("/recharge", async (req, res) => {
   var id = req.body.id;
   var kindID = req.body.kindID;
   //console.log(`insert into recarga values (${id_generated},current_timestamp,${id},'${kindID}');`)
@@ -80,28 +77,23 @@ app.post('/recharge', async (req,res) => {
   };
 });
 
-})
-app.post('/utilize', async (req,res) => {
+app.post("/utilize", async (req, res) => {
   let id = req.body.id;
-  recarga = await db.seeID(dbCredentials,id);
-  console.log(recarga)
+  recarga = await db.seeID(dbCredentials, id);
+  console.log(recarga);
   res.send({
-    recharges : recarga
-  })
-})
+    recharges: recarga,
+  });
+});
 
-
-
-app.post('/teste',(req,res) => {
-
-  console.log(req.body)
+app.post("/teste", (req, res) => {
+  console.log(req.body);
   res.send({
     id: "r23r23",
     message: "id criado",
   });
   res = {
-    id : "r23r23",
-    message: "id criado"
-  }
-})
-
+    id: "r23r23",
+    message: "id criado",
+  };
+});
