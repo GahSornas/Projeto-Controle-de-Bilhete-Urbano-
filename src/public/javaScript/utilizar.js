29e023b9c7716a9c8b16dd8cb16542afdedeb205
Gabriel <gabrielbsornas@gmail.com>
2022-12-03 16:17:18 -0300

    update front utilizar

--- src/public/javaScript/utilizar.js
+++ src/public/javaScript/utilizar.js
@@ -1,3 +1,15 @@
-function teste(id){
-    document.getElementById(id).style.display = "flex";
-} 
+//function teste(id) {
+//  document.getElementById(id).style.display = "flex";
+//}
+
+let tipoBilhete;
+let tempoRestante;
+
+let ulBilhetesAtivos = document.querySelector("#ulBilhetesAtivos");
+let bilheteAtivo = document.createElement("button");
+let pulaLinha = document.createElement("br");
+bilheteAtivo.appendChild(`${tipoBilhete}`);
+bilheteAtivo.appendChild(pulaLinha);
+bilheteAtivo.appendChild(`${tempoRestante}`);
+bilheteAtivo.classList.add("btnBilhetesAtivos");
+ulBilhetesAtivos.appendChild(bilheteAtivo);
