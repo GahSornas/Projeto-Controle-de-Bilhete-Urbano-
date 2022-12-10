9216ff9a4410fafbad7cb3fab4533e0ed200f250
manoel.acmn <manoel.acmn@puccampinas>
2022-12-10 04:26:47 -0300

    show id on click

--- src/public/javaScript/utilizar.js
+++ src/public/javaScript/utilizar.js
@@ -70,7 +70,12 @@ function printBilhetesDisponiveis(tipoBilhete, idBilhete) {
   let bilheteDisponivel = document.createElement("button");
   bilheteDisponivel.classList.add("btnBilhetes");
   bilheteDisponivel.id = idBilhete;
-  bilheteAtivo.onclick = function(){idBilheteClicado(this.id)};
+  bilheteDisponivel.setAttribute("onclick", "idBilheteClicado(this.id)");
+
+  // bilheteAtivo.setAttribute("onclick", "idBilheteClicado(this.id)");
+  // bilheteAtivo.onclick = function(){
+  //   idBilheteClicado(this.id)
+  // };
 
   let h3TipoBilhete = document.createElement("h3");
   h3TipoBilhete.innerHTML = tipoBilhete;
